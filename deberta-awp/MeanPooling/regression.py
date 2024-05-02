38642b6f489ce3b5a9ecd95043a5a5f819999417
unknown <2569618533@qq.com>
2024-05-01 20:59:36 -0700

    save pth

--- deberta-awp/MeanPooling/regression.py
+++ deberta-awp/MeanPooling/regression.py
@@ -12,6 +12,7 @@ from transformers import (
     AdamW,
     get_polynomial_decay_schedule_with_warmup
 )
+import os
 import pandas as pd
 import numpy as np
 import torch.nn as nn
@@ -162,6 +163,62 @@ def train(args):
             logits = outputs.logits
             loss = nn.MSELoss()(logits.view(-1), labels.view(-1))
             return (loss, outputs) if return_outputs else loss
+        
+        def _save_checkpoint(self, model, trial, metrics=None):
+            # In all cases, including ddp/dp/deepspeed, self.model is always a reference to the model we
+            # want to save except FullyShardedDDP.
+            # assert unwrap_model(model) is self.model, "internal model should be a reference to self.model"
+    
+            # Save model checkpoint
+            PREFIX_CHECKPOINT_DIR = "checkpoint"
+            checkpoint_folder = f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}"
+    
+            if self.hp_search_backend is None and trial is None:
+                self.store_flos()
+    
+            run_dir = self._get_output_dir(trial=trial)
+            output_dir = os.path.join(run_dir, checkpoint_folder)
+            # self.save_model(output_dir, _internal_call=True)  # 不要保存为HuggingFace的格式
+            os.makedirs(output_dir)
+            torch.save(model.state_dict(), f"{output_dir}/model-{self.state.global_step}.pth") # 保存为pth文件格式
+            
+    
+            if not self.args.save_only_model:
+                # Save optimizer and scheduler
+                self._save_optimizer_and_scheduler(output_dir)
+                # Save RNG state
+                self._save_rng_state(output_dir)
+    
+            # Determine the new best metric / best model checkpoint
+            if metrics is not None and self.args.metric_for_best_model is not None:
+                metric_to_check = self.args.metric_for_best_model
+                if not metric_to_check.startswith("eval_"):
+                    metric_to_check = f"eval_{metric_to_check}"
+                metric_value = metrics[metric_to_check]
+    
+                operator = np.greater if self.args.greater_is_better else np.less
+                if (
+                    self.state.best_metric is None
+                    or self.state.best_model_checkpoint is None
+                    or operator(metric_value, self.state.best_metric)
+                ):
+                    self.state.best_metric = metric_value
+                    self.state.best_model_checkpoint = output_dir
+    
+            # Save the Trainer state
+            # if self.args.should_save:
+            #     # Update the `TrainerControl` state to where we are currently
+            #     self.state.stateful_callbacks["TrainerControl"] = self.control.state()
+            #     self.state.save_to_json(os.path.join(output_dir, TRAINER_STATE_NAME))
+    
+            if self.args.push_to_hub:
+                self._push_from_checkpoint(output_dir)
+    
+            # Maybe delete some older checkpoints.
+            if self.args.should_save:
+                # Solely rely on numerical checkpoint id for rotation.
+                # mtime is not reliable especially on some fuse fs in cloud environments.
+                self._rotate_checkpoints(use_mtime=False, output_dir=run_dir)
 
         def training_step(self, model, inputs):
             """
@@ -293,8 +350,8 @@ def train(args):
 
     def compute_metrics(p):
         preds, labels = p
-        print(preds)
-        print(labels)
+        # print(preds)
+        # print(labels)
         score = cohen_kappa_score(
             labels,
             preds.clip(1, 6).round(),
@@ -352,7 +409,7 @@ def train(args):
         optimizers=(optimizer, scheduler),
         awp_lr=args.awp_lr,
         awp_eps=args.awp_eps,
-        awp_start_epoch=args.awp_start_epoch,
+        awp_start_epoch=args.awp_start_epoch
     )
 
     trainer.train()
