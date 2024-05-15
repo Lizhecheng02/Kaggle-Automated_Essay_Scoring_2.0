eb35789db4b36651bf4afaacf6139e6208909bee
unknown <2569618533@qq.com>
2024-05-15 13:22:50 -0700

    train lgbm

---
+++ tree/lgbm_main.py
@@ -0,0 +1,151 @@
+import gc
+import pandas as pd
+import warnings
+from train_tokenizer import train_tokenizer
+from config import CFG
+from preprocess import essay_preprocess
+from vectorizers import *
+from essay_processor import EssayProcessor
+from lgbm_train import train_lgbm
+warnings.filterwarnings("ignore")
+
+train = pd.read_csv("../dataset/train.csv")
+train = train[:1000]
+test = pd.read_csv("../dataset/test.csv")
+
+if CFG.DO_PREPROCESS:
+    print("DO_PREPROCESS!!!")
+    train = essay_preprocess(df=train)
+    test = essay_preprocess(df=test)
+    train.drop(columns=["full_text_no_punc"], axis=1, inplace=True)
+    test.drop(columns=["full_text_no_punc"], axis=1, inplace=True)
+
+if CFG.USE_EssayProcessor:
+    print("USE_EssayProcessor!!!")
+    essay_processor = EssayProcessor()
+    train_sent_agg_df = essay_processor.sentence_processor(df=train)
+    train_paragraph_agg_df = essay_processor.paragraph_processor(df=train)
+    train_word_agg_df = essay_processor.word_processor(df=train)
+
+    test_sent_agg_df = essay_processor.sentence_processor(df=test)
+    test_paragraph_agg_df = essay_processor.paragraph_processor(df=test)
+    test_word_agg_df = essay_processor.word_processor(df=test)
+
+    train_essay_agg_df = train.merge(
+        train_sent_agg_df,
+        on="essay_id",
+        how="left"
+    )
+    train_essay_agg_df = train_essay_agg_df.merge(
+        train_paragraph_agg_df,
+        on="essay_id",
+        how="left"
+    )
+    train_essay_agg_df = train_essay_agg_df.merge(
+        train_word_agg_df,
+        on="essay_id",
+        how="left"
+    )
+
+    test_essay_agg_df = test.merge(
+        test_sent_agg_df,
+        on="essay_id",
+        how="left"
+    )
+    test_essay_agg_df = test_essay_agg_df.merge(
+        test_paragraph_agg_df,
+        on="essay_id",
+        how="left"
+    )
+    test_essay_agg_df = test_essay_agg_df.merge(
+        test_word_agg_df,
+        on="essay_id",
+        how="left"
+    )
+
+    train_essay_agg_df.drop(
+        columns=[
+            "word", "sent", "paragraph",
+            "essay_id", "full_text", "score"
+        ],
+        axis=1,
+        inplace=True
+    )
+    test_essay_agg_df.drop(
+        columns=[
+            "word", "sent", "paragraph",
+            "essay_id", "full_text"
+        ],
+        axis=1,
+        inplace=True
+    )
+
+    print("The shape of train_essay_agg_df is:", train_essay_agg_df.shape)
+    print("The shape of test_essay_agg_df is:", test_essay_agg_df.shape)
+
+    train.drop(
+        columns=["word", "sent", "paragraph"],
+        axis=1,
+        inplace=True
+    )
+    train = pd.concat([train, train_essay_agg_df], axis=1)
+    print("The shape of train after essay processor is:", train.shape)
+    test.drop(
+        columns=["word", "sent", "paragraph"],
+        axis=1,
+        inplace=True
+    )
+    test = pd.concat([test, test_essay_agg_df], axis=1)
+    print("The shape of test after essay processor is:", test.shape)
+
+if CFG.TRAIN_TOKENIZER:
+    print("TRAIN_TOKENIZER!!!")
+    tokenizer, df_train, df_test = train_tokenizer(train=train, test=test)
+    train = pd.merge(train, df_train, on="essay_id", how="left")
+    print("The shape of train after training new tokenizer is:", train.shape)
+    test = pd.merge(test, df_test, on="essay_id", how="left")
+    print("The shape of test after training new tokenizer is:", test.shape)
+
+if CFG.USE_ORIGINAL_TFIDF_VECTORIZER:
+    print("USE_ORIGINAL_TFIDF_VECTORIZER!!!")
+    tf_idf_vectorizer, df_train = use_tf_idf_vectorizer(train=train)
+    train = pd.merge(train, df_train, on="essay_id", how="left")
+    print("The shape of train after using original tf-idf vectorizer is:", train.shape)
+    df_test = use_tf_idf_vectorizer_for_test(
+        vectorizer=tf_idf_vectorizer,
+        test=test
+    )
+    test = pd.merge(test, df_test, on="essay_id", how="left")
+    print("The shape of test after using original tf-idf vectorizer is:", test.shape)
+
+if CFG.USE_ORIGINAL_COUNT_VECTORIZER:
+    print("USE_ORIGINAL_COUNT_VECTORIZER!!!")
+    count_vectorizer, df_train = use_count_vectorizer(train=train)
+    train = pd.merge(train, df_train, on="essay_id", how="left")
+    print("The shape of train after using original count vectorizer is:", train.shape)
+    df_test = use_count_vectorizer_for_test(
+        vectorizer=count_vectorizer,
+        test=test
+    )
+    test = pd.merge(test, df_test, on="essay_id", how="left")
+    print("The shape of test after using original count vectorizer is:", test.shape)
+
+train.drop(["essay_id", "full_text"], axis=1, inplace=True)
+train.fillna(0.0, inplace=True)
+has_null_train = train.isnull().values.any()
+print(has_null_train)
+
+test.drop(["essay_id", "full_text"], axis=1, inplace=True)
+test.fillna(0.0, inplace=True)
+has_null_test = test.isnull().values.any()
+print(has_null_test)
+
+print("The shape of final train is:", train.shape)
+print("The shape of final test is:", test.shape)
+
+gc.collect()
+
+lgbm_models, lgbm_model_nums, lgbm_predictions = train_lgbm(
+    X_train=train,
+    X_test=test
+)
---
+++ tree/lgbm_train.py
@@ -0,0 +1,102 @@
+import gc
+import pandas as pd
+import numpy as np
+import warnings
+import lightgbm as lgb
+from metrics import qwk_obj, quadratic_weighted_kappa
+from lightgbm import log_evaluation, early_stopping
+from tqdm import tqdm
+from config import CFG
+from sklearn.metrics import accuracy_score, cohen_kappa_score
+from sklearn.model_selection import StratifiedKFold
+warnings.filterwarnings("ignore")
+
+
+def train_lgbm(X_train, X_test):
+    oof = []
+    models = []
+
+    X = X_train
+    y = X_train["score"].values
+    feature_names = list(filter(lambda x: x not in ["score"], X_train.columns))
+
+    reloaded_test = pd.read_csv("../dataset/test.csv")
+    prediction = reloaded_test[["essay_id"]].copy()
+
+    skf = StratifiedKFold(
+        n_splits=CFG.lgbm_n_split,
+        random_state=CFG.random_state,
+        shuffle=True
+    )
+
+    callbacks = [
+        log_evaluation(period=CFG.lgbm_log_evaluation),
+        early_stopping(
+            stopping_rounds=CFG.lgbm_stopping_rounds,
+            first_metric_only=True
+        )
+    ]
+    for fold_id, (train_idx, val_idx) in tqdm(enumerate(skf.split(X.copy(), y.copy().astype(str))), total=5):
+        model = lgb.LGBMRegressor(
+            objective=qwk_obj,
+            metrics="None",
+            learning_rate=CFG.lgbm_learning_rate,
+            n_estimators=CFG.lgbm_n_estimators,
+            max_depth=CFG.lgbm_max_depth,
+            num_leaves=CFG.lgbm_num_leaves,
+            reg_alpha=CFG.lgbm_reg_alpha,
+            reg_lambda=CFG.lgbm_reg_lambda,
+            colsample_bytree=CFG.lgbm_colsample_bytree,
+            random_state=CFG.random_state,
+            verbosity=CFG.lgbm_verbosity
+        )
+        X_train_tmp = X_train.iloc[train_idx][feature_names]
+        y_train_tmp = X_train.iloc[train_idx]["score"] - CFG.a
+        X_val_tmp = X_train.iloc[val_idx][feature_names]
+        y_val_tmp = X_train.iloc[val_idx]["score"] - CFG.a
+
+        print("\n==== Fold_{} Training ====\n".format(fold_id + 1))
+        lgb_model = model.fit(
+            X_train_tmp,
+            y_train_tmp,
+            eval_names=["train", "valid"],
+            eval_set=[(X_train_tmp, y_train_tmp), (X_val_tmp, y_val_tmp)],
+            eval_metric=quadratic_weighted_kappa,
+            callbacks=callbacks
+        )
+
+        pred_val = lgb_model.predict(
+            X_val_tmp,
+            num_iteration=lgb_model.best_iteration_
+        )
+        df_tmp = X_train.iloc[val_idx][["score"]].copy()
+        df_tmp["pred"] = pred_val + CFG.a
+
+        oof.append(df_tmp)
+        models.append(model.booster_)
+        lgb_model.booster_.save_model(f"lgbm/fold_{fold_id}.txt")
+
+    df_oof = pd.concat(oof)
+    acc = accuracy_score(df_oof["score"], df_oof["pred"].clip(1, 6).round())
+    kappa = cohen_kappa_score(
+        df_oof["score"],
+        df_oof["pred"].clip(1, 6).round(),
+        weights="quadratic"
+    )
+    print("Acc:", acc)
+    print("Kappa:", kappa)
+
+    prediction["score"] = 0
+    pred_test = models[0].predict(X_test[feature_names]) + CFG.a
+    for i in range(CFG.lgbm_n_split - 1):
+        pred_now = models[i + 1].predict(X_test[feature_names]) + CFG.a
+        pred_test = np.add(pred_test, pred_now)
+    pred_test = pred_test / 5.0
+    pred_test = pred_test.clip(1, 6).round()
+    print(pred_test)
+
+    prediction["score"] = pred_test
+    prediction.to_csv("submission_lgbm.csv", index=False)
+    print(prediction.head(3))
+
+    return models, len(models), prediction
