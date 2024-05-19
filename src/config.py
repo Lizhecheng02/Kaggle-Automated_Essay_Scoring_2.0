8b21baed6eb6c773e12c0c6dca8bf9ffaacffd29
unknown <2569618533@qq.com>
2024-05-19 15:15:50 -0700

    update

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ class CFG:
 
     # you can choose from "mean_pooling", "meanmax_pooling", "attention_pooling", "weighted_layer_pooling", "concat_pooling", "lstm_pooling", "gru_pooling"
     pooling_type = "mean_pooling"
-    backbone_model = "microsoft/deberta-v3-small"
+    backbone_model = "microsoft/deberta-v3-base"
     max_length = 1536
     use_wandb = True
 
@@ -12,18 +12,18 @@ class CFG:
     zero_dropout = True
 
     # initialization parameters
-    use_initialization = True
+    use_initialization = False
     initializer_range = 1.0
 
     # whether to use rubric
-    use_rubric = True
+    use_rubric = False
 
     # replace \n\n
     is_replace = True
 
     # cross validation, you can choose from "only4k", "only13k", "only17k", "only19k", "only30k", "train17k_validation4k"
-    validation_type = "train17k_validation4k"
-    num_split = 4
+    validation_type = "only17k"
+    num_split = 5
     selected_fold_id = 0
 
     # attention_pooling
