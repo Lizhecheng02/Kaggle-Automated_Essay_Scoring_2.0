8310c88e420cc558c42d45d4f8e23f65faa23f2f
unknown <2569618533@qq.com>
2024-05-26 16:03:02 -0700

    xgb config

--- tree/config.py
+++ tree/config.py
@@ -44,11 +44,11 @@ class CFG:
 
     # a and b for metrics
     # 17k
-    # a = 2.998 
-    # b = 1.092
+    a = 2.998 
+    b = 1.092
     # 19k
-    a = 2.99575151546552
-    b = 0.9725680876911655
+    # a = 2.99575151546552
+    # b = 0.9725680876911655
 
     # original tf-idf vectorizer
     tfidf_vectorizer_ngram_range = (2, 3)
@@ -64,13 +64,26 @@ class CFG:
 
     # parameters for lgbm
     lgbm_n_split = 5
-    lgbm_log_evaluation = 50
-    lgbm_stopping_rounds = 500
+    lgbm_log_evaluation = 25
+    lgbm_stopping_rounds = 100
     lgbm_learning_rate = 0.01
-    lgbm_n_estimators = 3000
-    lgbm_max_depth = 8
-    lgbm_num_leaves = 10
-    lgbm_reg_alpha = 0.5
-    lgbm_reg_lambda = 0.7
-    lgbm_colsample_bytree = 0.7
+    lgbm_n_estimators = 2048
+    lgbm_max_depth = 10
+    lgbm_num_leaves = 15
+    lgbm_reg_alpha = 2
+    lgbm_reg_lambda = 0.1
+    lgbm_colsample_bytree = 0.3
     lgbm_verbosity = 1
+
+    # parameters for xgboost
+    xgb_n_split = 5
+    xgb_log_evaluation = 25
+    xgb_stopping_rounds = 100
+    xgb_learning_rate = 0.01
+    xgb_n_estimators = 2048
+    xgb_max_depth = 10
+    xgb_num_leaves = 15
+    xgb_reg_alpha = 2
+    xgb_reg_lambda = 0.1
+    xgb_colsample_bytree = 0.3
+    xgb_verbosity = 1
